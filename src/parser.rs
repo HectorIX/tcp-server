92a125220db3e056bd6ac5db3f848099a64a7162
HectorIX <giorgoschiotis@protonmail.com>
2017-07-11 20:37:42 +0100

    Upload Completed - No session-key

--- src/parser.rs
+++ src/parser.rs
@@ -25,7 +25,7 @@ pub fn parser( instruction:String ) -> bool {
 }
 
 
-pub fn request_splitter( client_request:String ) -> (String,String) {
+pub fn request_splitter( client_request:String ) -> (String, String) {
 
     let req_vector: Vec<&str> = client_request.split("::").collect();
     let (the_state,request) = (req_vector[0].to_string(), req_vector[1].to_string());
@@ -34,7 +34,7 @@ pub fn request_splitter( client_request:String ) -> (String,String) {
 }
 
 
-pub fn data_splitter( request_service:String ) -> (String,String) {
+pub fn data_splitter( request_service:String ) -> (String, String) {
 
     let req_vector: Vec<&str> = request_service.split("**").collect();
     let (service,data) = (req_vector[0].to_string(), req_vector[1].to_string());
@@ -43,10 +43,19 @@ pub fn data_splitter( request_service:String ) -> (String,String) {
 }
 
 
-pub fn split_credentials(data:String) -> (String,String)  {
+pub fn split_credentials(data:String) -> (String, String)  {
 
     let data_vector: Vec<&str> = data.split("--").collect();
     let (username,password) = (data_vector[0].to_owned(), data_vector[1].to_owned());
 
-    (username,password)
+    (username, password)
+}
+
+
+pub fn split_filename_from_context(data:String) -> (String, String)  {
+
+    let data_vector: Vec<&str> = data.split("<$$>").collect();
+    let (filename, file_context) = (data_vector[0].to_owned(), data_vector[1].to_owned());
+
+    (filename, file_context)
 }
--- src/upload.rs
+++ src/upload.rs
@@ -1,6 +1,22 @@
+use file_io;
+use parser;
 
 
 pub fn upload_service(data:String) -> String {
 
-    "upload".to_string()
+    let mut path = "private_data/".to_string();
+
+    let (username, rest_data) = parser::split_credentials(data);
+    println!("rest_data = {:?}", rest_data.clone() );
+    let (filename, file_context) = parser::split_filename_from_context(rest_data.clone());
+
+
+    path.push_str(&username);
+    path.push_str("/");
+    path.push_str(&filename);
+
+
+    file_io::write_file(path, file_context);
+
+    "Upload completed!".to_string()
 }
