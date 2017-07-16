b743ef22c034815b815c294983a2c2c5039b3f84
HectorIX <giorgoschiotis@protonmail.com>
2017-07-16 19:02:19 +0100

    Download + Session Key

--- src/download.rs
+++ src/download.rs
@@ -1,5 +1,6 @@
 use file_io;
 use parser;
+use user;
 
 
 pub fn download_service( data: String ) -> String {
@@ -10,22 +11,34 @@ pub fn download_service( data: String ) -> String {
 
     if data.len() > 0 {
 
-        let (username, filename) = parser::split_credentials(data);
+        let (username, rest_data) = parser::split_credentials(data);
+        let (session_key, filename) = parser::split_session_key(rest_data);
 
 
         path.push_str(&username);
         path.push_str("/");
         path.push_str(&filename);
 
-        let file_context = file_io::read_file(path.to_string());
+        if (username == user::get_username()) &
+           (session_key == user::get_session_key()) {
 
-        let mut file_request = "download_state::OK**".to_string();
+            let file_context = file_io::read_file(path.to_string());
 
-        file_request.push_str(&filename);
-        file_request.push_str("<$$>");
-        file_request.push_str(&file_context);
+            let mut file_request = "download_state::OK**".to_string();
 
-        file_request
+
+            file_request.push_str(&user::get_session_key());
+            file_request.push_str("#!?#");
+            file_request.push_str(&filename);
+            file_request.push_str("<$$>");
+            file_request.push_str(&file_context);
+
+            file_request
+        }
+        else {
+
+            "download_state::SESSION_Expired**".to_string()
+        }
     }
     else {
 
